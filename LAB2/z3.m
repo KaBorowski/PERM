d748fd9e21d3bf36d5af656d2372edc238046149
KozlowskiSzymon <szymon2kozlowski@gmail.com>
2019-04-08 13:15:24 +0200

    Add files via upload

--- LAB2/z3.m
+++ LAB2/z3.m
@@ -1,10 +1,11 @@
 Fs = 200000;
 t = (0:length(syg)-1)/Fs;
 Wn = 0.4; %cut-off freq.
-N = 50;   %
+N = 50;   %order - how steep
+spectrogram(syg, 128, 64, [], Fs, 'yaxis')
 
 bhi = fir1(N,Wn,'high',chebwin(N+1,50));
-freqz(bhi,1)
+freqz(bhi,1);
 outhi = filter(bhi,1,syg);
 
 subplot(3,1,1)
@@ -18,3 +19,6 @@ xlabel('Time (s)')
 
 subplot(3,1,3)
 spectrogram(outhi, 128, 64, [], Fs, 'yaxis')
+
+
+
